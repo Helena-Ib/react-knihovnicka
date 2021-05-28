1456c8d9550aadd975d5fabe8f373bb3cbf1a918
Helena Ibarra <helena8ibarra@gmail.com>
2021-05-28 21:33:51 +0200

    knihovna je hotova

---
+++ src/index.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+import Book from './Book';
+import { books } from './books.js';
+
+const App = () => {
+  const [library, setLibrary] = useState(books);
+
+  const update = (id) => {
+    console.log(id);
+
+    const newLibrary = [].concat(library);
+    const book = newLibrary.find((book) => book.id === id);
+    book.read = !book.read;
+    setLibrary(newLibrary);
+  };
+
+  return (
+    <>
+      <header className="header">
+        <h1>Moje knihovnicka</h1>
+        <div className="icon">
+          <img src="/assets/book.svg" alt="" />
+        </div>
+      </header>
+
+      <p>Celkem přečteno knih: {library.filter((book) => book.read).length} </p>
+      <div className="library">
+        {library.map((book) => (
+          <Book
+            key={book.id}
+            id={book.id}
+            title={book.title}
+            author={book.author}
+            year={book.year}
+            cover={book.cover}
+            read={book.read}
+            handleUpdate={update}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+render(<App />, document.querySelector('#app'));
